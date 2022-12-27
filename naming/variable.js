8a366193fbea1260e56eb3da253a96393b516448
TokuyamaR <qpflapper.1007@gmail.com>
2022-12-27 16:19:06 +0900

    chore: 20.変数名

---
+++ naming/variable.js
@@ -0,0 +1,15 @@
+// BAD:listは配列を想起させるので不適切
+const averageList = {
+  japanese: 70,
+  math: 100,
+  society: 80,
+  english: 60,
+};
+
+// GOOD:
+const AVERAGE_SCORE = {
+  japanese: 70,
+  math: 100,
+  society: 80,
+  english: 60,
+};
