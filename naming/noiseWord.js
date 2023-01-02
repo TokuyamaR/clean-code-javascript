c77142e4ced782ac507333882ee0e0156a84d31f
TokuyamaR <qpflapper.1007@gmail.com>
2023-01-02 23:58:55 +0900

    chore: 26.意味のない単語を含めない

---
+++ naming/noiseWord.js
@@ -0,0 +1,11 @@
+// BAD:重複した意味は命名が冗長となり不要
+const descriptionText = "説明文";
+const coldIceCreamFlavorList = ["チョコ", "バニラ", "ミント"];
+const verticalAxisLine = "y-axis";
+const mouthColorOnFace = "red";
+
+// GOOD
+const description = "説明文";
+const iceCreamFlavorList = ["チョコ", "バニラ", "ミント"];
+const verticalAxis = "y-axis";
+const mouthColor = "red";
