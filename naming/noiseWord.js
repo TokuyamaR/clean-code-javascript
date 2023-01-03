900c4ae874e1f4a09ba046324e40a6d5e2464b66
TokuyamaR <qpflapper.1007@gmail.com>
2023-01-03 23:59:52 +0900

    docs:コメント

--- naming/noiseWord.js
+++ naming/noiseWord.js
@@ -4,7 +4,7 @@ const coldIceCreamFlavorList = ["チョコ", "バニラ", "ミント"];
 const verticalAxisLine = "y-axis";
 const mouthColorOnFace = "red";
 
-// GOOD
+// GOOD:不要なものを消す
 const description = "説明文";
 const iceCreamFlavorList = ["チョコ", "バニラ", "ミント"];
 const verticalAxis = "y-axis";
